2151aacba3bdb873117bc6539a5e098dbb4151f6
Yashpb1513 <yashbrahmbhatt26@gmail.com>
2022-08-04 22:37:53 +0530

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./utils/theme";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-      <Navbar/>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </ThemeProvider>
-     
     </div>
   );
 }
