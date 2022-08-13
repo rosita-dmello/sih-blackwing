d5eae7eda5fa8ccb26df7bc72a971ce88fe943f5
blackchapel <kc.dark@protonmail.com>
2022-08-13 11:29:58 +0530

    bidder crud

--- src/services/bidder.service.js
+++ src/services/bidder.service.js
@@ -1,6 +1,63 @@
 const Bidder = require('../models/bidder.schema');
-const sendOtpByEmail = require('./auth.service');
+const { sendOtpByEmail, sendOtpBySms } = require('./auth.service');
 const verifyGstin = require('./verification.service');
+
+const bidderList = async (searchText, pageNo, pageSize) => {
+    let result;
+    let bidders;
+    const queryObj = { isDeleted: false };
+
+    let aggregationPipeline = [];
+    let searchObj;
+    let skip, limit;
+
+    if (searchText && searchText !== 'undefined') {
+        searchObj = {
+            'index': 'bidder',
+            'text': {
+                'query': searchText,
+                'path': ['companyName', 'preferenceCategory', 'registeredAddress', 'partners', 'bidderType', 'city', 'state', 'country', 'panNumber', 'gstinNumber', 'establishmentYear', 'natureOfBusiness', 'legalStatus', 'companyCategory', 'contactName']
+            }
+        };
+
+        aggregationPipeline.push({ $search: searchObj });
+    }
+
+    aggregationPipeline.push({ $match: queryObj });
+
+    if (pageNo && pageSize) {
+        if (pageNo < 1 || pageSize < 1) {
+            limit = pageSize;
+            skip = ((pageNo - 1) * pageSize);
+            aggregationPipeline.push({ $skip: skip });
+            aggregationPipeline.push({ $limit: limit });
+        }
+    }
+
+    bidders = await Bidder.aggregate(aggregationPipeline).collation({ locale: 'en_US' });
+
+    result = {
+        message: 'Bidder List',
+        data: {
+            bidders
+        }
+    };
+    return result;
+}
+
+const bidderById = async (req) => {
+    let result;
+
+    const bidder = await Bidder.findById(req.params.id);
+
+    result = {
+        message: 'Bidder details',
+        data: {
+            bidder
+        }
+    };
+    return result;
+}
 
 const bidderCreate = async (req) => {
     let result;
@@ -12,19 +69,22 @@ const bidderCreate = async (req) => {
 
     if (!panVerification) {
         result = {
-            message: 'Enter correct PAN'
+            message: 'Enter correct PAN',
+            error: true
         }
         information = false;
     }
     if (!gstinVerfication) {
         result = {
-            message: 'Enter correct GSTIN'
+            message: 'Enter correct GSTIN',
+            error: true
         }
         information = false;
     }
     if (!panVerification && !gstinVerfication) {
         result = {
-            message: 'Enter correct PAN & GSTIN'
+            message: 'Enter correct PAN & GSTIN',
+            error: true
         }
         information = false;
     }
@@ -34,16 +94,48 @@ const bidderCreate = async (req) => {
 
     await newBidder.save(req);
 
-    const authId = await sendOtpByEmail(req, newBidder);
+    const authEmailId = await sendOtpByEmail(req, newBidder);
+    const authSmsId = await sendOtpBySms(req, newBidder);
 
     result = {
         message: 'Bidder successfully created',
         data: { 
-            authId,
+            authEmailId,
+            authSmsId,
             newBidder
         }
     };
     return result;
 };
 
-module.exports = bidderCreate;
+const bidderUpdate = async (req) => {
+    let result;
+
+    const bidder = await Bidder.findByIdAndUpdate(req.params.id, req.body);
+
+    result = {
+        message: 'Bidder updated successfully',
+        data: bidder
+    };
+    return result;
+};
+
+const bidderDelete = async (req) => {
+    let result;
+
+    const bidder = await Bidder.findByIdAndUpdate(req.params.id, { isDeleted: true });
+
+    result = {
+        message: 'Bidder deleted successfully',
+        data: bidder
+    };
+    return result;
+};
+
+module.exports = {
+    bidderList,
+    bidderById,
+    bidderCreate,
+    bidderUpdate,
+    bidderDelete
+};
