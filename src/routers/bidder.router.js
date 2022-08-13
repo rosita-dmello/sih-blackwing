eb68cdb3e65dfe09e185ea25a2610c74c90dcce1
blackchapel <kc.dark@protonmail.com>
2022-08-13 11:29:45 +0530

    bidder routes

--- src/routers/bidder.router.js
+++ src/routers/bidder.router.js
@@ -1,8 +1,16 @@
-const { createBidder } = require('./../controllers/bidder.controller');
+const { createBidder, getBidderById, getBidderList, updateBidder, deleteBidder } = require('./../controllers/bidder.controller');
 const express = require("express");
 
 const router = new express.Router();
 
+router.get('/', getBidderList);
+
+router.get('/:id', getBidderById);
+
 router.post('/', createBidder);
+
+router.put('/:id', updateBidder);
+
+router.delete('/:id', deleteBidder);
 
 module.exports = router;
