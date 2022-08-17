807724e368401ee3cce1ff7890eba1349fb02db1
Vidhita Pai <optimus@192.168.1.39>
2022-08-18 00:39:28 +0530

    tender basic crud implemented

---
+++ src/services/tender.service.js
@@ -0,0 +1,104 @@
+const Tender = require('../models/tender.schema');
+
+const tenderList = async (searchText, pageNo, pageSize) => {
+    let result, tenders;
+    const queryObj = { isDeleted: false };
+
+    let aggregationPipeline = [];
+    let searchObj;
+    let skip, limit;
+
+    if (searchText && searchText !== 'undefined') {
+        searchObj = {
+            'index': 'tender',
+            'text': {
+                'query': searchText,
+                'path': ['tenderreferencenumber', 'tendertype', 'tendercategory', 'nitdescription', 'worktitle', 'workdescription', 'productcategory', 'productsubcategory', 'contracttype', 'locationdetail', 'prebidmeetingplace', 'bidopeningplace', 'invitingofficer']
+            }
+        };
+
+        aggregationPipeline.push({ $search: searchObj });
+    }
+
+    aggregationPipeline.push({ $match: queryObj });
+
+    if (pageNo && pageSize) {
+        if (pageNo < 1 || pageSize < 1) {
+            limit = pageSize;
+            skip = ((pageNo - 1) * pageSize);
+            aggregationPipeline.push({ $skip: skip });
+            aggregationPipeline.push({ $limit: limit });
+        }
+    }
+
+    tenders = await Tender.aggregate(aggregationPipeline).collation({ locale: 'en_US' });
+
+    result = {
+        message: 'Tender List',
+        data: {
+            tenders
+        }
+    };
+    return result;
+}
+
+const tenderById = async (req) => {
+    let result;
+
+    const tender = await Tender.findById(req.params.id);
+
+    result = {
+        message: 'Tender details',
+        data: {
+            tender
+        }
+    };
+    return result;
+}
+
+const tenderCreate = async (req) => {
+    let result;
+    const newTender = new Tender(req.body);
+
+    await newTender.save();
+
+    result = {
+        message: 'Tender successfully created',
+        data: {
+            newTender
+        }
+    };
+    return result;
+};
+
+const tenderUpdate = async (req) => {
+    let result;
+
+    const tender = await Tender.findByIdAndUpdate(req.params.id, req.body);
+
+    result = {
+        message: 'Tender updated successfully',
+        data: tender
+    };
+    return result;
+};
+
+const tenderDelete = async (req) => {
+    let result;
+
+    const tender = await Tender.findByIdAndUpdate(req.params.id, { isDeleted: true });
+
+    result = {
+        message: 'Tender deleted successfully',
+        data: tender
+    };
+    return result;
+};
+
+module.exports = {
+    tenderList,
+    tenderById,
+    tenderCreate,
+    tenderUpdate,
+    tenderDelete
+};
