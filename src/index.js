b5a4dca817929a3ef24838e0000b07f724ae01fe
blackchapel <kc.dark@protonmail.com>
2022-08-18 20:01:32 +0530

    corrected api path

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // API Routes
-app.use('api/auth', authRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/bidder', bidderRoutes);
 
 // Test API
