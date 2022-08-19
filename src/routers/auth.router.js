2eadc519490f9901c681edeb60eb967888a8f276
blackchapel <kc.dark@protonmail.com>
2022-08-19 09:13:53 +0530

    login route

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,14 +1,23 @@
-const  { verifyEmailAndMobile, generateTotpSecret, enableTotp, verifyTotpToken } = require('./../controllers/auth.controller');
-const express = require("express");
+const express = require('express');
+const auth = require('./../middlewares/authentication.middleware');
+const  { 
+    verifyEmailAndMobile, 
+    generateTotpSecret, 
+    enableTotp, 
+    verifyTotpToken, 
+    login 
+} = require('./../controllers/auth.controller');
 
 const router = new express.Router();
 
 router.post('/verify/otp', verifyEmailAndMobile);
 
-router.get('/totp/:id', generateTotpSecret);
+router.get('/totp/:id', auth.verifyJwtToken, generateTotpSecret);
 
-router.put('/totp/:id', enableTotp);
+router.put('/totp/:id', auth.verifyJwtToken, enableTotp);
 
 router.post('/totp/:id', verifyTotpToken);
+
+router.post('/login', login);
 
 module.exports = router;
