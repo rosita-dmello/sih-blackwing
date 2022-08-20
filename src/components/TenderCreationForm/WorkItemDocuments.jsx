7f3fb78be437b93f37aa449797c7329d31c8ccf4
harsh0989 <harshmangukiya1506@gmail.com>
2022-08-20 21:42:06 +0530

    Tender Creation page, work documents comp created

---
+++ src/components/TenderCreationForm/WorkItemDocuments.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Grid,Button } from '@mui/material'
+
+const WorkItemDocuments = (props) => {
+    return (
+        <>WorkItemDocuments
+            <Grid container>
+                <Grid item>
+                    <Button onClick={props.prevStep}>previous</Button>
+                </Grid>
+                <Grid item>
+                    <Button onClick={props.nextStep}>next</Button>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default WorkItemDocuments
