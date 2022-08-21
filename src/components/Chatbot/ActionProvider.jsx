edaeba9aa3277a5c8d32023b69994f7e3d6c5d91
Rosita <esorthe7@gmail.com>
2022-08-21 19:12:04 +0530

    chat bot config

--- src/components/Chatbot/ActionProvider.jsx
+++ src/components/Chatbot/ActionProvider.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 const ActionProvider = ({ createChatBotMessage, setState, children }) => {
+    const handleMessage = (message) => {
+        const botMessage = createChatBotMessage(message);
+    
+        setState((prev) => ({
+          ...prev,
+          messages: [...prev.messages, botMessage],
+        }));
+      };
   return (
     <div>
       {React.Children.map(children, (child) => {
         return React.cloneElement(child, {
-          actions: {},
+          actions: {
+              handleMessage
+          },
         });
       })}
     </div>
