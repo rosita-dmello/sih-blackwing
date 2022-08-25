0a164dc02b5125c12dba473799551362d0b9c806
harsh0989 <harshmangukiya1506@gmail.com>
2022-08-26 03:23:59 +0530

    changes in work item details

--- src/components/TenderCreationForm/WorkItemDetails.jsx
+++ src/components/TenderCreationForm/WorkItemDetails.jsx
@@ -17,7 +17,7 @@ const WorkItemDetails = (props) => {
             <CustomTextField textlabel="Work/Item Title" value="workitemtitle" values={workItemDetails} setValues={setWorkItemDetails} />
           </Grid>
           <Grid item xs={6}>
-            <CustomTextField textlabel="Location Detail" value="locationDetails" values={workItemDetails} setValues={setWorkItemDetails} />
+            <CustomTextField textlabel="Location Detail" value="locationdetails" values={workItemDetails} setValues={setWorkItemDetails} />
           </Grid>
           <Grid item xs={6} sx={{ width: '100%' }} >
             <CustomTextField textlabel="Work/Item Description" multiline rows={2} value="workitemdesc" values={workItemDetails} setValues={setWorkItemDetails} />
@@ -26,29 +26,33 @@ const WorkItemDetails = (props) => {
             <CustomTextField textlabel="Pincode" value="pincode" values={workItemDetails} setValues={setWorkItemDetails} />
           </Grid>
           <Grid item xs={6} sx={{ width: '100%' }} >
-            <CustomTextField textlabel="Pre Qualification Details" multiline rows={2} value="preQualDetails" values={workItemDetails} setValues={setWorkItemDetails} />
+            <CustomTextField textlabel="Pre Qualification Details" multiline rows={2} value="prequaldetails" values={workItemDetails} setValues={setWorkItemDetails} />
           </Grid>
           <Grid item xs={6}>
-            <CustomTextField textlabel="Pre Bid Meeting" value="locationDetails" values={workItemDetails} setValues={setWorkItemDetails} />
+            <Grid item sx={{ marginLeft: '5%' }}>
+              <CustomSelect textlabel="Pre Bid Meeting" array={preBid} value="prebidmeeting" values={workItemDetails} setValues={setWorkItemDetails} />
+            </Grid>
+
           </Grid>
           <Grid item xs={6} sx={{ width: '100%' }} >
-            <CustomDropDown textlabel="Product Category" value="productCategory" array={productCategories} values={workItemDetails} setValues={setWorkItemDetails} />
+            <CustomDropDown textlabel="Product Category" value="productcategory" array={productCategories} values={workItemDetails} setValues={setWorkItemDetails} />
           </Grid>
           <Grid item xs={6} >
-            <Grid item sx={{marginLeft:'5%'}}>
-              <CustomSelect textlabel="Pre Bid Meeting" array={preBid} value="preBidMeeting" values={workItemDetails} setValues={setWorkItemDetails} />
-            </Grid>
-            <CustomTextField disable={workItemDetails.preBidMeeting} textlabel="Location Detail" value="locationDetails" values={workItemDetails} setValues={setWorkItemDetails} />
+            <CustomTextField textlabel="Pre Bid Meeting Place" value="prebidmeetingplace" values={workItemDetails} setValues={setWorkItemDetails} />
+            <CustomTextField textlabel="Pre Bid Meeting Address" multiline rows={2} value="prebidmeetingadress" values={workItemDetails} setValues={setWorkItemDetails} />
           </Grid>
         </Grid>
 
       </Box>
-      <Grid container>
-        <Grid item>
+      <Grid container direction="column" sx={{
+                padding: "3rem"
+            }}>
+        <Grid item sx={{
+                    display: "flex",
+                    justifyContent: "space-between"
+                }}>
           <Button onClick={props.prevStep}>previous</Button>
-        </Grid>
-        <Grid item>
-          <Button onClick={()=>{props.nextStep(WorkItemDetails)}}>next</Button>
+          <Button onClick={() => { props.nextStep(WorkItemDetails) }}>next</Button>
         </Grid>
       </Grid>
     </>
