e56e730b840539de219627f8a1fa90b0fe7ba1fb
harsh0989 <harshmangukiya1506@gmail.com>
2022-08-26 01:35:13 +0530

    changes in basic details, Boq dept

--- src/components/TenderCreationForm/BoqDepartment.jsx
+++ src/components/TenderCreationForm/BoqDepartment.jsx
@@ -30,7 +30,7 @@ import TagsInput from "../TagsInput";
 import moment from "moment";
 import { Add, Remove } from "@mui/icons-material";
 moment().format();
-function BoqDepartment() {
+function BoqDepartment(props) {
   const [formValues, setFormValues] = useState([
     {
       itemdescription: "",
@@ -50,13 +50,13 @@ function BoqDepartment() {
 
   let addFormFields = () => {
     setFormValues([...formValues, {
-        itemdescription: "",
-        quantity: "",
-        units: "",
-        rate: "",
-        preferredmodels: "",
-        warranty: "",
-      }]);
+      itemdescription: "",
+      quantity: "",
+      units: "",
+      rate: "",
+      preferredmodels: "",
+      warranty: "",
+    }]);
   };
 
   let removeFormFields = (i) => {
@@ -68,187 +68,183 @@ function BoqDepartment() {
   let handleSubmit = (event) => {
     event.preventDefault();
     console.log(formValues);
-}
+  }
   return (
-    <Box
-      sx={{
-        marginTop: 8,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        width: "70%",
-      }}
-    >
-      <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
-        <ReceiptLongIcon />
-      </Avatar>
-      <Typography
-        component="h1"
-        variant="h5"
-        fontWeight="bold"
-        sx={{
-          mt: 2,
-        }}
-      >
-        Bill Of Quantities (BOQ for Financial Bid)
-      </Typography>
-      <Box component="form" noValidate sx={{ mt: 3, width: "100%" }} onSubmit={handleSubmit}>
-        <Grid
-          container
-          direction="column"
-          sx={{
-            width: "100%",
-          }}
-          spacing={2}
-        >
-          {formValues.map((element, index) => (
-            <Grid item xs={12} key={index}>
-              <Card
-                sx={{
-                  padding: "2rem",
-                  width: "100%",
-                  borderRadius: 0,
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-                elevation={2}
-              >
-                <Grid
-                  container
-                  direction="column"
-                  spacing={1}
-                  sx={{
-                    width: "100%",
-                    border: "1px solid #243665",
-                    padding: 3,
-                  }}
-                >
-                  <Grid item xs={12}>
-                    <TextField
-                      type="text"
-                      name="itemdescription"
-                      label="Item Description"
-                      value={element.itemdescription || ""}
-                      fullWidth
-                      multiline
-                      rows={2}
-                      onChange={(e) => handleChange(index, e)}
-                      size="small"
-                    />
-                  </Grid>
-                  <Grid item xs={6}>
-                    <TextField
-                      type="number"
-                      name="quantity"
-                      label="Quantity"
-                      value={element.quantity || ""}
-                      fullWidth
-                      onChange={(e) => handleChange(index, e)}
-                      size="small"
-                    />
-                  </Grid>
-                  <Grid item xs={6}>
-                    <TextField
-                      name="units"
-                      label="Units"
-                      value={element.units || ""}
-                      fullWidth
-                      onChange={(e) => handleChange(index, e)}
-                      size="small"
-                    />
-                  </Grid>
+    <Box sx={{ width: '80%', margin: '3vh auto' }}>
+      <Box sx={{display:'flex',justifyContent:'center'}}>
 
-                  <Grid item xs={12}>
-                    <TextField
-                      type="number"
-                      name="rate"
-                      label="Estimated Rate"
-                      value={element.rate || ""}
-                      fullWidth
-                      onChange={(e) => handleChange(index, e)}
-                      size="small"
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-              <TagsInput
-               onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
-                selectedTags={(items) => {
-                    let newFormValues = [...formValues];
-                    newFormValues[index]["preferredmodels"] = items;
-                    setFormValues(newFormValues);
-                }
-                }
-                margin="normal"
-                fullWidth
-                variant="outlined"
-                id="preferredmodels"
-                name="preferredmodels"
-                placeholder="Enter names of Make/Models"
-                label="Preferred Make/Models"
-                color="primary"
-                size="small"
-                defaultValue={formValues[index]["preferredmodels"]}
-              />
-            </Grid>
-                  <Grid item xs={12}>
-                    <TextField
-                      name="warranty"
-                      label="Minimum Warranty Requirement (optional)"
-                      value={element.warranty || ""}
-                      fullWidth
-                      onChange={(e) => handleChange(index, e)}
-                      size="small"
-                    />
-                  </Grid>
-                  <Grid item xs={12} sx={{
+        <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
+          <ReceiptLongIcon />
+        </Avatar>
+        <Typography
+          component="h1"
+          variant="h5"
+          fontWeight="bold"
+          sx={{
+            mt: 2,
+          }}
+        >
+          Bill Of Quantities (BOQ for Financial Bid)
+        </Typography>
+      </Box>
+        <Box component="form" noValidate sx={{ mt: 3, width: "100%" }} onSubmit={handleSubmit}>
+          <Grid
+            container
+            direction="column"
+            sx={{
+              width: "100%",
+            }}
+            spacing={2}
+          >
+            {formValues.map((element, index) => (
+              <Grid item xs={12} key={index}>
+                <Card
+                  sx={{
+                    padding: "2rem",
+                    width: "100%",
+                    borderRadius: 0,
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                  elevation={2}
+                >
+                  <Grid
+                    container
+                    direction="column"
+                    spacing={1}
+                    sx={{
+                      width: "100%",
+                      border: "1px solid #3e92cc",
+                      padding: 3,
+                    }}
+                  >
+                    <Grid item xs={12}>
+                      <TextField
+                        type="text"
+                        name="itemdescription"
+                        label="Item Description"
+                        value={element.itemdescription || ""}
+                        fullWidth
+                        multiline
+                        rows={2}
+                        onChange={(e) => handleChange(index, e)}
+                        size="small"
+                      />
+                    </Grid>
+                    <Grid item xs={6}>
+                      <TextField
+                        type="number"
+                        name="quantity"
+                        label="Quantity"
+                        value={element.quantity || ""}
+                        fullWidth
+                        onChange={(e) => handleChange(index, e)}
+                        size="small"
+                      />
+                    </Grid>
+                    <Grid item xs={6}>
+                      <TextField
+                        name="units"
+                        label="Units"
+                        value={element.units || ""}
+                        fullWidth
+                        onChange={(e) => handleChange(index, e)}
+                        size="small"
+                      />
+                    </Grid>
+
+                    <Grid item xs={12}>
+                      <TextField
+                        type="number"
+                        name="rate"
+                        label="Estimated Rate"
+                        value={element.rate || ""}
+                        fullWidth
+                        onChange={(e) => handleChange(index, e)}
+                        size="small"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TagsInput
+                        onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
+                        selectedTags={(items) => {
+                          let newFormValues = [...formValues];
+                          newFormValues[index]["preferredmodels"] = items;
+                          setFormValues(newFormValues);
+                        }
+                        }
+                        margin="normal"
+                        fullWidth
+                        variant="outlined"
+                        id="preferredmodels"
+                        name="preferredmodels"
+                        placeholder="Enter names of Make/Models"
+                        label="Preferred Make/Models"
+                        color="primary"
+                        size="small"
+                        defaultValue={formValues[index]["preferredmodels"]}
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        name="warranty"
+                        label="Minimum Warranty Requirement (optional)"
+                        value={element.warranty || ""}
+                        fullWidth
+                        onChange={(e) => handleChange(index, e)}
+                        size="small"
+                      />
+                    </Grid>
+                    <Grid item xs={12} sx={{
                       display: "flex",
                       flexDirection: "row-reverse"
-                  }}>
-                  {index ? (
-                    <Button
-                      type="button"
-                      className="button remove"
-                      onClick={() => removeFormFields(index)}
-                      variant="outlined"
-                      color="error"
-                      size="small"
-                      startIcon={<Remove/>}
-                    >
-                      Remove
-                    </Button>
-                  ) : null}
-                </Grid>
-                </Grid>
-                
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-        <Box className="button-section">
-          <Button
-            className="button add"
-            type="button"
-            variant="contained"
-            onClick={() => addFormFields()}
-            sx={{
+                    }}>
+                      {index ? (
+                        <Button
+                          type="button"
+                          className="button remove"
+                          onClick={() => removeFormFields(index)}
+                          variant="outlined"
+                          color="error"
+                          size="small"
+                          startIcon={<Remove />}
+                        >
+                          Remove
+                        </Button>
+                      ) : null}
+                    </Grid>
+                  </Grid>
+
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+          <Box className="button-section">
+            <Button
+              className="button add"
+              type="button"
+              variant="contained"
+              onClick={() => addFormFields()}
+              sx={{
                 mt: 2
-            }}
-            startIcon={<Add/>}
-          >
-            Add Item
-          </Button>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Submit
-          </Button>
+              }}
+              startIcon={<Add />}
+            >
+              Add Item
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Button onClick={props.prevStep}>previous</Button>
+              </Grid>
+              <Grid item>
+                <Button onClick={() => { props.nextStep(formValues) }}>next</Button>
+              </Grid>
+            </Grid>
+          </Box>
         </Box>
       </Box>
-    </Box>
+
   );
 }
 
