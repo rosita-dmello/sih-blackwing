e629a0ac61f9a4eb4314d656d7d66e55bdb594c3
harsh0989 <harshmangukiya1506@gmail.com>
2022-08-25 10:04:06 +0530

    changes in fee details workitem details, tender creation

--- src/components/TenderCreationForm/FeeDetails.jsx
+++ src/components/TenderCreationForm/FeeDetails.jsx
@@ -1,9 +1,66 @@
-import React from 'react'
-import { Grid,Button } from '@mui/material'
+import React, { useState } from 'react'
+import { Grid, Button, Box, Typography, Checkbox, FormGroup } from '@mui/material'
+import CustomSelect from './CustomSelect'
+import CustomTextField from './CustomTextField'
 
 const FeeDetails = (props) => {
+  const [feeDetails, setFeeDetails] = useState([])
+  const paymentMode = [{ value: 'offline', label: 'offline' }, { value: 'online', label: 'online' }, { value: 'Not Applicable', label: 'Not Applicable' }]
+  const EMDfee = [{ value: 'fixed', label: 'fixed' }, { value: 'percentage', label: 'percentage' }]
+  const extemption = [{ label: 'Yes', value: true }, { label: 'No', value: false }]
+  const offlineInstruments=[{label:'DD-Demand Draft',value:false,name:'demandDraft'},]
+  console.log(feeDetails)
+  const handleChange=(e)=>{
+    const name=e.target.name
+    console.log(name)
+  }
   return (
-    <>FeeDetails
+
+    <>
+      <Box sx={{ width: '85%', margin: '3vh auto' }}>
+        <Typography variant='h5'>Tender Fee Details</Typography>
+        <hr style={{ width: '100%' }}></hr>
+        <Grid container sx={{ width: '100%' }}>
+          <Grid item xs={12}>
+            <CustomSelect array={paymentMode} value="feePaymentMode" textlabel="Fee Payment Mode" values={feeDetails} setValues={setFeeDetails} />
+          </Grid>
+          <Grid item xs={12} >
+            <Grid item xs={12} sx={{ marginLeft: '-5%' }}>
+              <CustomTextField textlabel="Tender Fee(INR)" value="tenderfee" values={feeDetails} setValues={setFeeDetails} />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomSelect array={extemption} value="extemptionAllowed" textlabel="Extemption Allowed" values={feeDetails} setValues={setFeeDetails} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box sx={{ width: '85%', margin: '3vh auto' }}>
+        <Typography variant='h5'>Earnest Money Deposit (EMD) Details</Typography>
+        <hr style={{ width: '100%' }}></hr>
+        <Grid container sx={{ width: '100%' }}>
+          <Grid item xs={12}>
+            <CustomSelect array={EMDfee} value="emdFee" textlabel="EMD Fee" values={feeDetails} setValues={setFeeDetails} />
+          </Grid>
+          <Grid item xs={12} >
+            <Grid item xs={12} sx={{ marginLeft: '-5%' }}>
+              <CustomTextField textlabel="EMD Amount(INR)" value="emdAmount" values={feeDetails} setValues={setFeeDetails} />
+            </Grid>
+            <Grid item xs={12} sx={{ marginLeft: '-5%' }}>
+              <CustomTextField textlabel="EMD ECV %" value="emdECV" values={feeDetails} setValues={setFeeDetails} />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomSelect array={extemption} value="emdExtemption" textlabel="EMD Extemption Allowed" values={feeDetails} setValues={setFeeDetails} />
+            </Grid>
+            <Grid item xs={12} sx={{ marginLeft: '-5%' }}>
+              <CustomTextField textlabel="EMD Fee Payable to" value="emdFeePayTo" values={feeDetails} setValues={setFeeDetails} />
+            </Grid>
+            <Grid item xs={12} sx={{ marginLeft: '-5%' }}>
+              <CustomTextField textlabel="EMD Fee Payable At" value="emdFeePayAt" values={feeDetails} setValues={setFeeDetails} />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+      
       <Grid container>
         <Grid item>
           <Button onClick={props.prevStep}>previous</Button>
@@ -11,7 +68,8 @@ const FeeDetails = (props) => {
         <Grid item>
           <Button onClick={props.nextStep}>next</Button>
         </Grid>
-      </Grid></>
+      </Grid>
+    </>
   )
 }
 
--- src/components/TenderCreationForm/WorkItemDetails.jsx
+++ src/components/TenderCreationForm/WorkItemDetails.jsx
@@ -7,10 +7,11 @@ import CustomSelect from './CustomSelect'
 const WorkItemDetails = (props) => {
   const [workItemDetails, setWorkItemDetails] = useState({})
   console.log(workItemDetails)
+  const preBid = [{ label: 'Yes', value: true }, { label: 'No', value: false }]
   const productCategories = ['Computer- Data Processing', 'Computer- H/W', 'Computer- Man Power', 'Computer- S/W', 'Crop Products', 'Electrical Work/Equipment', 'Electronics Equipment', 'Food Products', 'Furniture/Fixture', 'Medical Equipments/Waste', 'Medicines', 'Metal Makes', 'Miscellaneous', 'Pesticide', 'Publishing/Printing']
   return (
     <>
-      <Box sx={{ width: '85%', margin: '3vh auto' }}>
+      <Box sx={{ width: '80%', margin: '3vh auto' }}>
         <Grid container sx={{ width: '100%' }}>
           <Grid item xs={6} sx={{ width: '100%' }} >
             <CustomTextField textlabel="Work/Item Title" value="workitemtitle" values={workItemDetails} setValues={setWorkItemDetails} />
@@ -31,10 +32,12 @@ const WorkItemDetails = (props) => {
             <CustomTextField textlabel="Pre Bid Meeting" value="locationDetails" values={workItemDetails} setValues={setWorkItemDetails} />
           </Grid>
           <Grid item xs={6} sx={{ width: '100%' }} >
-            <CustomDropDown textlabel="Product Category" value="productCategory" array={productCategories} values={workItemDetails} setValues={setWorkItemDetails}  />
+            <CustomDropDown textlabel="Product Category" value="productCategory" array={productCategories} values={workItemDetails} setValues={setWorkItemDetails} />
           </Grid>
-          <Grid item xs={6}>
-            <CustomSelect textlabel="Pre Bid Meeting" value="preBidMeeting" values={workItemDetails} setValues={setWorkItemDetails}/>
+          <Grid item xs={6} >
+            <Grid item sx={{ marginLeft: '-6%' }}>
+              <CustomSelect textlabel="Pre Bid Meeting" array={preBid} value="preBidMeeting" values={workItemDetails} setValues={setWorkItemDetails} />
+            </Grid>
             <CustomTextField disable={workItemDetails.preBidMeeting} textlabel="Location Detail" value="locationDetails" values={workItemDetails} setValues={setWorkItemDetails} />
           </Grid>
         </Grid>
