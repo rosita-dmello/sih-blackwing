48d26a9269da65c877a8e189d4833ffebb3811b6
Rosita <esorthe7@gmail.com>
2022-08-26 11:41:52 +0530

    add graphs

--- src/components/FinancialBid.jsx
+++ src/components/FinancialBid.jsx
@@ -20,11 +20,11 @@ const FinancialBid = ({ tender }) => {
   const [bidderOffer, setBidderOffer] = useState([]);
   let handleChange = (i, e) => {
     let newFormValues = [...bidderOffer];
-    if(!newFormValues[i]) {
+    if (!newFormValues[i]) {
       newFormValues[i] = {};
-    } 
+    }
     newFormValues[i][e.target.name] = e.target.value;
-    console.log(newFormValues)
+    console.log(newFormValues);
     setBidderOffer(newFormValues);
   };
 
@@ -32,7 +32,7 @@ const FinancialBid = ({ tender }) => {
   //   if (tender) {
   //     setBoq(tender.boq)
   //   }
-    
+
   // }, [])
   return (
     <Grid
@@ -42,148 +42,167 @@ const FinancialBid = ({ tender }) => {
       }}
       spacing={2}
     >
-      {boq && boq.map((item, index) => {
-        return (
-          <Grid item xs={12}>
-            <Card>
-              <CardContent>
-                <Box
-                  sx={{
-                    border: "1px solid #243665",
-                    padding: 3,
-                  }}
-                >
-                  <Typography variant="h5" fontWeight={"bold"} color="primary">
-                    {item.itemdescription}
-                  </Typography>
-                  <Grid container spacing={1}>
-                    {["rate", "quantity", "units"].map((key) => {
-                      return (
-                        <Grid item xs={12}>
+      {boq &&
+        boq.map((item, index) => {
+          return (
+            <Grid item xs={12}>
+              <Card>
+                <CardContent>
+                  <Box
+                    sx={{
+                      border: "1px solid #243665",
+                      padding: 3,
+                    }}
+                  >
+                    <Typography
+                      variant="h5"
+                      fontWeight={"bold"}
+                      color="primary"
+                    >
+                      {item.itemdescription}
+                    </Typography>
+                    <Grid container spacing={1}>
+                      {["rate", "quantity", "units"].map((key) => {
+                        return (
+                          <Grid item xs={12}>
+                            <Grid container spacing={1} sx={{ mt: "0.05rem" }}>
+                              <Grid item xs={4}>
+                                <Typography variant="body1">
+                                  {_.startCase(key)}
+                                </Typography>
+                              </Grid>
+                              <Grid item xs={6}>
+                                <Typography variant="body1" fontWeight={"bold"}>
+                                  {item[key]}
+                                </Typography>
+                              </Grid>
+                            </Grid>
+                          </Grid>
+                        );
+                      })}
+                      {item.warranty && (
+                        <Grid item>
                           <Grid container spacing={1} sx={{ mt: "0.05rem" }}>
                             <Grid item xs={4}>
                               <Typography variant="body1">
-                                {_.startCase(key)}
+                                {"Warranty Requirement"}
                               </Typography>
                             </Grid>
                             <Grid item xs={6}>
                               <Typography variant="body1" fontWeight={"bold"}>
-                                {item[key]}
+                                {item.warranty}
                               </Typography>
                             </Grid>
                           </Grid>
                         </Grid>
-                      );
-                    })}
-                    {item.warranty && (
-                      <Grid item>
-                        <Grid container spacing={1} sx={{ mt: "0.05rem" }}>
-                          <Grid item xs={4}>
-                            <Typography variant="body1">
-                              {"Warranty Requirement"}
-                            </Typography>
-                          </Grid>
-                          <Grid item xs={6}>
-                            <Typography variant="body1" fontWeight={"bold"}>
-                              {item.warranty}
-                            </Typography>
+                      )}
+                      {item.preferredmodels && (
+                        <Grid item xs={12}>
+                          <Grid container spacing={1} sx={{ mt: "0.05rem" }}>
+                            <Grid item xs={4}>
+                              <Typography variant="body1">
+                                Preferred Models
+                              </Typography>
+                            </Grid>
+                            <Grid item xs={6}>
+                              <Typography variant="body1" fontWeight={"bold"}>
+                                {item.preferredmodels}
+                              </Typography>
+                            </Grid>
                           </Grid>
                         </Grid>
+                      )}
+
+                      <Grid item xs={6}>
+                        <TextField
+                          type="number"
+                          name="unitrate"
+                          label="Unit Rate"
+                          value={
+                            bidderOffer[index] && bidderOffer[index].unitrate
+                          }
+                          fullWidth
+                          onChange={(e) => handleChange(index, e)}
+                          size="small"
+                        />
                       </Grid>
-                    )}
-                    {item.preferredmodels && (
+                      <Grid item xs={6}>
+                        <TextField
+                          name="totalamount"
+                          fullWidth
+                          id="totalamount"
+                          label="Total Amount"
+                          inputProps={{ readOnly: true }}
+                          value={
+                            bidderOffer[index] &&
+                            bidderOffer[index].unitrate * item.quantity
+                          }
+                          size="small"
+                          onChange={(e) => handleChange(index, e)}
+                          placeholder=""
+                        />
+                      </Grid>
+
                       <Grid item xs={12}>
-                        <Grid container spacing={1} sx={{ mt: "0.05rem" }}>
-                          <Grid item xs={4}>
-                            <Typography variant="body1">
-                              Preferred Models
-                            </Typography>
-                          </Grid>
-                          <Grid item xs={6}>
-                            <Typography variant="body1" fontWeight={"bold"}>
-                              {item.preferredmodels}
-                            </Typography>
-                          </Grid>
-                        </Grid>
+                        <TagsInput
+                          onKeyPress={(e) => {
+                            e.key === "Enter" && e.preventDefault();
+                          }}
+                          selectedTags={(items) => {
+                            let newFormValues = [...bidderOffer];
+                            newFormValues[index] &&
+                              (newFormValues[index]["offeredmodels"] = items);
+                            setBidderOffer(newFormValues);
+                          }}
+                          margin="normal"
+                          fullWidth
+                          variant="outlined"
+                          id="offeredmodels"
+                          name="offeredmodels"
+                          placeholder="Enter names of Make/Models"
+                          label="Offered Make/Models"
+                          color="primary"
+                          size="small"
+                        />
                       </Grid>
-                    )}
-                   
-                    <Grid item xs={6} >
-                      <TextField
-                        type="number"
-                        name="unitrate"
-                        label="Unit Rate"
-                        value={
-                          bidderOffer[index] && bidderOffer[index].unitrate
-                        }
-                        fullWidth
-                        onChange={(e) => handleChange(index, e)}
-                        size="small"
-                      />
-                    </Grid>
-                    <Grid item xs={6}>
-                      <TextField
-                        name="totalamount"
-                        fullWidth
-                        id="totalamount"
-                        label="Total Amount"
-                        inputProps={{ readOnly: true }}
-                        value={
-                          bidderOffer[index] &&
-                          (bidderOffer[index].unitrate * item.quantity)
-                        }
-                        size="small"
-                        onChange={(e) => handleChange(index, e)}
-                        
-                      />
-                    </Grid>
 
-                    <Grid item xs={12}>
-                      <TagsInput
-                        onKeyPress={(e) => {
-                          e.key === "Enter" && e.preventDefault();
-                        }}
-                        selectedTags={(items) => {
-                          let newFormValues = [...bidderOffer];
-                          newFormValues[index] &&
-                            (newFormValues[index]["offeredmodels"] = items);
-                          setBidderOffer(newFormValues);
-                        }}
-                        margin="normal"
-                        fullWidth
-                        variant="outlined"
-                        id="offeredmodels"
-                        name="offeredmodels"
-                        placeholder="Enter names of Make/Models"
-                        label="Offered Make/Models"
-                        color="primary"
-                        size="small"
-                      />
+                      <Grid item xs={12}>
+                        <TextField
+                          name="warranty"
+                          label="Warranty Offered (optional)"
+                          value={
+                            bidderOffer[index] && bidderOffer[index].warranty
+                          }
+                          fullWidth
+                          onChange={(e) => handleChange(index, e)}
+                          size="small"
+                        />
+                      </Grid>
                     </Grid>
-
-                    <Grid item xs={12}>
-                      <TextField
-                        name="warranty"
-                        label="Warranty Offered (optional)"
-                        value={
-                          bidderOffer[index] && bidderOffer[index].warranty
-                        }
-                        fullWidth
-                        onChange={(e) => handleChange(index, e)}
-                        size="small"
-                      />
-                    </Grid>
-                    
-                  </Grid>
-                  
-                </Box>
-              </CardContent>
-            </Card>
-            <Button onClick={(e) => console.log(bidderOffer)}>Submit</Button>
-          </Grid>
-        );
-      })}
-      
+                  </Box>
+                </CardContent>
+              </Card>
+            </Grid>
+          );
+        })}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row-reverse",
+        }}
+      >
+        <Button
+          onClick={(e) => console.log(bidderOffer)}
+          variant="contained"
+          sx={{
+            borderRadius: 0,
+            ml: 2,
+            mt: 2
+          }}
+        >
+          Submit
+        </Button>
+      </Box>
     </Grid>
   );
 };
